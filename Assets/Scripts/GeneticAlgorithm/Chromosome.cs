9058f9f3c98a256618f39df8bd33276c2bd7110d
Sebastian Idziak <i.basti@wp.pl>
2020-01-08 00:43:13 +0100

    Implemented Genetic Algorithm

--- Assets/Scripts/GeneticAlgorithm/Chromosome.cs
+++ Assets/Scripts/GeneticAlgorithm/Chromosome.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Chromosome
 {
@@ -12,8 +13,43 @@ public class Chromosome
         var Dummy = new Chromosome();
         for (int i = 0; i < chromosomeSize; i++)
         {
+            Dummy.genes.Add(Random.Range(0f, 1f));
+        }
+        return Dummy;
+    }
+
+    public static Chromosome GenerateRandomNormalChromosome()
+    {
+        var Dummy = new Chromosome();
+        for (int i = 0; i < chromosomeSize; i++)
+        {
             Dummy.genes.Add(RandomFromDistribution.RandomRangeNormalDistribution(0, 1, RandomFromDistribution.ConfidenceLevel_e._999));
         }
         return Dummy;
+    }
+
+    public Chromosome GenesCopy()
+    {
+        return new Chromosome
+        {
+            genes = new List<float>(this.genes),
+            timeScore = this.timeScore,
+            collisionScore = this.collisionScore
+        };
+    }
+
+    public void Mutate()
+    {
+        for (int i = 0; i < genes.Count; i++)
+        {
+            genes[i] += RandomFromDistribution.RandomRangeNormalDistribution(-.125f, .125f, RandomFromDistribution.ConfidenceLevel_e._999);
+            genes[i] = Mathf.Clamp(genes[i], 0f, 1f);
+        }
+    }
+
+    public void ClearScore()
+    {
+        timeScore = 0f;
+        collisionScore = 0f;
     }
 }
--- Assets/Scripts/GeneticAlgorithm/Generation.cs
+++ Assets/Scripts/GeneticAlgorithm/Generation.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Generation
 {
-    public readonly static int generationSize = 5;
+    public readonly static int generationSize = 8;
     public List<Chromosome> population = new List<Chromosome>();
 
     public void LoadGeneration(List<RuleSet> rules)
     {
+        population = new List<Chromosome>();
         foreach (var rule in rules)
         {
             population.Add(
@@ -29,9 +32,67 @@ public class Generation
         }
         return dummy;
     }
-    ///TODO: FINISH THIS
+
     public Generation GenerateNextGeneration()
     {
-        return GenerateRandomGeneration();
+        var toRet = new Generation();
+        var orderedPopulation = population.OrderByDescending(c => c.timeScore / (c.collisionScore + 1f)).Select(c => c.GenesCopy()).ToList();
+
+        // selection
+        var pruned = Selection(orderedPopulation);
+
+        // crossover
+        toRet.population = new List<Chromosome>(pruned);
+        while (toRet.population.Count != orderedPopulation.Count)
+        {
+            Chromosome child1, child2;
+            (child1, child2) = Breed(pruned[Random.Range(0, pruned.Count)], pruned[Random.Range(0, pruned.Count)]);
+            toRet.population.Add(child1);
+            if (orderedPopulation.Count - toRet.population.Count > 1)
+            {
+                toRet.population.Add(child2);
+            }
+        }
+
+        // mutation
+        foreach (var chromosome in toRet.population)
+        {
+            chromosome.Mutate();
+        }
+
+        foreach (var chromosome in toRet.population)
+        {
+            chromosome.ClearScore();
+        }
+
+        return toRet;
+    }
+
+    public List<Chromosome> Selection(List<Chromosome> orderedPopulation)
+    {
+        var pruned = new List<Chromosome>();
+        for (int i = 0; i < orderedPopulation.Count; i++)
+        {
+            if (Random.value > i / (float)orderedPopulation.Count)
+            {
+                pruned.Add(orderedPopulation[i]);
+            }
+        }
+        return pruned;
+    }
+
+    public (Chromosome, Chromosome) Breed(Chromosome partent1, Chromosome partent2)
+    {
+        int crossoverPoint = Random.Range(0, Chromosome.chromosomeSize);
+        var child1 = new Chromosome();
+        var child2 = new Chromosome();
+        child1.genes = partent1.genes.Take(crossoverPoint).Union(partent2.genes.Skip(crossoverPoint)).ToList();
+        child2.genes = partent2.genes.Take(crossoverPoint).Union(partent1.genes.Skip(crossoverPoint)).ToList();
+        return (child1, child2);
+    }
+
+    public List<Chromosome> FillUpWithCrossover(List<Chromosome> population)
+    {
+        return null;
     }
 }
